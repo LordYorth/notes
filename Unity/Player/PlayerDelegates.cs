fd85e1ce571cb061b064e5d7a5145093d8c97dd5
LordYorth <83911465+LordYorth@users.noreply.github.com>
2021-05-09 10:40:20 -0400

    Create PlayerDelegates.cs

---
+++ Unity/Player/PlayerDelegates.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDelegates : MonoBehaviour {
+  
+  enum State{
+    Idle,
+    Busy,
+    Dead
+  }
+  
+  private state = State.Idle;
+  
+  private Action attackFunction;
+  
+  private void Start() {
+    attackFunction = PunchAttack;
+  }
+  
+  private void Update() {
+    switch (state) {
+    default:
+    case State.Idle:
+      HandleMovement();
+      HandleAttack();
+      break;
+    case State.Busy:
+      HandleAttack();
+      break;
+    case State.Dead:
+      break;
+    }
+  }
+  
+  private void SetUseSword() {
+    attackFunction = SwordAttack;
+  }
+  
+  private void HandleAttack() {
+    if (Input.GetMouseButtonDown(0)) {
+      attackFunction();
+    }
+  }
+  
+  private void PunchAttack(){
+  }
+  
+  private void SwordAttack(){
+  
+  }
+}
