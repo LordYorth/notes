492ae242bb4785de2c8ea8da821f66cb924a101b
LordYorth <83911465+LordYorth@users.noreply.github.com>
2021-05-09 09:33:55 -0400

    Create Testing.cs

---
+++ Testing.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+  
+  [SerializeField] private ActionOnTimer actionOnTimer;
+  
+  private void Start() {
+  
+    actionOnTimer.SetTimer(1f, () => { Debug.Log("Timer complete!"); });
+  }
+}
